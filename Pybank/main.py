74b94d3f277ca540f6a40f0fcbd4b119b3c15af9
Npack08 <Nicobarzotti92@gmail.com>
2023-01-17 20:39:03 -0800

    git commit

--- Pybank/main.py
+++ Pybank/main.py
@@ -11,9 +11,12 @@ budget_csv= os.path.join("Resources","budget_data.csv")
 month_total = []
 Profit_Loss = []
 profit_change = []
-
-greatest_increase = 0
-greatest_decrease = 0
+month_of_change = []
+greatest_increase = ["",0]
+greatest_decrease = ["",99999999999]
+net_change = 0
+net_prev = 0
+total_net = 0
 
 #Read CSV file for data
 with open(budget_csv, "r") as csvfile:
@@ -21,25 +24,39 @@ with open(budget_csv, "r") as csvfile:
 
     #Read the header row first
     csv_header = next(csvreader)
-    
-    for row in csvreader:
+    first_row = next(csvreader)
+    total_net += int(first_row[1])
+    net_prev = int(first_row[1]) 
+
+for row in csvreader:
       
         month_total.append(row[0])
-        total_months= len(month_total)
+        month_total= len(month_total)
+        net_change = int(row[1]) - net_prev
+        net_prev = int(row[1])
+        month_of_change += [row[0]]
+
+        if net_change > greatest_increase[1]:
+            greatest_increase[0] = row[0]
+            greatest_increase[1] = net_change
+
         
+        if net_change < greatest_decrease[1]:
+            greatest_decrease[0] = row[0]
+            greatest_decrease[1] = net_change
+    
+
         Profit_Loss.append(int(row[1]))
         total = sum(Profit_Loss)
         
-        greatest_increase = (Profit_Loss[row[1] + 1])
-        print(greatest_increase)
-        
 # Create a loop to find changes in Profit and Loss
-    Variable_A = (Profit_Loss[i + 1] - Profit_Loss[i] for i in range(len(Profit_Loss)-1))
-    Average = sum(Variable_A) / int(len(Profit_Loss))
-    print(Average)
+Variable_A = (Profit_Loss[i + 1] - Profit_Loss[i] for i in range(len(Profit_Loss)-1))
+Average = sum(Variable_A) / int(len(Profit_Loss))
+print(Average)
 
-
-
+output = (f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n"
+f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
+print(output)
 
 
 

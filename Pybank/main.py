eb88b7b1afebf509fadaf68ed34e0e6ae90ab9f4
Npack08 <Nicobarzotti92@gmail.com>
2023-01-18 23:39:54 -0800

    git commit

--- Pybank/main.py
+++ Pybank/main.py
@@ -14,29 +14,30 @@ month_of_change = []
 greatest_increase = ["",0]
 greatest_decrease = ["",99999999999]
 net_change = 0
-net_prev = 0
-total_net = 0
+total = 0
 
 #Read CSV file for data
 with open(budget_csv, "r") as csvfile:
     csvreader= csv.reader(csvfile, delimiter=",")
 
     #Read the header row first
-    
     first_row = next(csvreader)
-    
+    first_row_data = next(csvreader)
+    net_prev = int(first_row_data[1])
+    total += first_row_data[1]
+
 # Loop through the variable list
     for row in csvreader:
         month_total.append(row[0])
         total_months = len(month_total) 
-        total = sum(Profit_Loss) 
+        total += int(row[1])
         net_change = int(row[1]) - net_prev
-        net_prev = int(row[1])
+        net_prev = int(first_row_data[1])
         month_of_change += [row[0]]
-        Profit_Loss.append(int(row[1]))
+        Profit_Loss.append(net_change)
         total = sum(Profit_Loss)
 
-        # Create an if statement to calculate the greatest increas and decrease in profit
+        # Create an if statement to calculate the greatest increase and decrease in profit
         if net_change > greatest_increase[1]:
             greatest_increase[0] = row[0]
             greatest_increase[1] = net_change
@@ -46,16 +47,16 @@ with open(budget_csv, "r") as csvfile:
             greatest_decrease[0] = row[0]
             greatest_decrease[1] = net_change
     
-
-        
-        
+    
 # Create a loop to find average change in Profit and Loss
-Variable_A = (Profit_Loss[i + 1] - Profit_Loss[i] for i in range(len(Profit_Loss)-1))
-Average = round(sum(Variable_A) / int(len(Profit_Loss)),2)
+Variable_A = [Profit_Loss[i + 1] - Profit_Loss[i] for i in range(len(Profit_Loss)-1)]
+Average = round(sum(Profit_Loss) / len(Profit_Loss),2)
 
 
 output = (f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n"
 f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
+
+# Print results
 
 print("Financial Analysis")
 print("------------------------")
@@ -66,6 +67,7 @@ print(output)
 
 #Set variable for output file
 # Transfer this data into the analysis file 
+
 output_file = "Analysis.csv"
 
 # Write the result to the Analysis.csv
